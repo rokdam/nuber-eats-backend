58664bc6a15e8f2e0fdf7c4b1494c0f01665e00c
rokdam <travel_dob@naver.com>
2022-02-02 17:40:14 +0900

    validation 실습

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -5,8 +5,14 @@ export class Restaurant {
 
     @Field(is => String)
     name: string;
-    
+
     @Field(is =>Boolean, {nullable:true})
-    isGood?: boolean;
+    isVegan?: boolean;
+
+    @Field(type => String)
+    address: string;
+
+    @Field(type => String)
+    ownerName: string;
 
 }
