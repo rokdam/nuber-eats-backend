79ddd5e6e6512edaba71611581f910724247d26b
rokdam <travel_dob@naver.com>
2022-02-02 17:15:09 +0900

    쿼리 리턴타입 변경, 인수속성 변경

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,12 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+@ObjectType()
+export class Restaurant {
+
+    @Field(is => String)
+    name: string;
+    
+    @Field(is =>Boolean, {nullable:true})
+    isGood?: boolean;
+
+}
