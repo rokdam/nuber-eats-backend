cd3451de1629e56b33740593fb3b82f85581c71b
rokdam <travel_dob@naver.com>
2022-02-20 22:19:12 +0900

    typeORM entity 적용

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,18 +1,33 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { number } from "joi";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType()
+@Entity()
 export class Restaurant {
 
+    @PrimaryGeneratedColumn()
+    @Field(type=> Number)
+    id: number;
+
     @Field(is => String)
+    @Column()
     name: string;
 
     @Field(is =>Boolean, {nullable:true})
+    @Column()
     isVegan?: boolean;
 
     @Field(type => String)
+    @Column()
     address: string;
 
     @Field(type => String)
+    @Column()
     ownerName: string;
+
+    @Field(type => String)
+    @Column()
+    categoryName: string;
 
 }
